fc8325863c03e9b1e7ed2c7644231045688cbf33
Enzo FRAIOLI <enzo.fraioli@viacesi.fr>
2024-11-11 14:09:54 +0100

    Add menu close functionality and update styles for menu management

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM Loaded');
-    const menuElements = document.querySelectorAll('.menuElementContainer');
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM Loaded");
 
-    menuElements.forEach(element => {
-        element.addEventListener('click', () => {
-            console.log('Clicked on:', element);
-            // Remove 'active' class from all elements
-            menuElements.forEach(el => el.classList.remove('active'));
+  const menuElements = document.querySelectorAll(".menuElementContainer");
 
-            // Add 'active' class to the clicked element
-            element.classList.add('active');
-        });
+  menuElements.forEach((element) => {
+    element.addEventListener("click", () => {
+      console.log("Clicked on:", element);
+
+      // Remove 'active' class from all elements
+
+      menuElements.forEach((el) => el.classList.remove("active"));
+
+      // Add 'active' class to the clicked element
+
+      element.classList.add("active");
     });
+  });
 });
+
+
+
+const menuGestion = document.getElementById("menuGestion");
+
+const menuClose = document.getElementById("menuClose");
+
+function closeMenuGestion() {
+  if (menuGestion && menuClose) {
+    menuGestion.style.display = "none";
+    menuClose.style.display = "flex";
+  } else {
+    console.log("No menuGestion or menuClose found");
+  }
+}
+
+function openMenuGestion() {
+  if (menuGestion && menuClose) {
+    menuGestion.style.display = "block";
+    menuClose.style.display = "none";
+  } else {
+    console.log("No menuGestion or menuClose found");
+  }
+}
